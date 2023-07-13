7e66927486c62abb0a76c521ebc3ec59c124cd20
jacbn <jabrown405@gmail.com>
2023-07-13 16:15:45 +0100

    finished grid

--- src/app/components/gridSquare.tsx
+++ src/app/components/gridSquare.tsx
@@ -5,7 +5,7 @@ import { GridImageProps } from "./gridImage";
 export interface GridSquareProps {
   title: string;
   description: string;
-  year: number;
+  year: string;
   lang: string;
   colour: string;
   image: GridImageProps;
@@ -13,14 +13,31 @@ export interface GridSquareProps {
   podiumNum?: number;
 }
 
-export default function GridSquare({title, description, year, lang, colour, image, link, podiumNum = 0} : GridSquareProps) {
-  // let cardClass = "card transition" + (podiumNum == 0 ? "" : " podium" + podiumNum);
-  const cardClass = `${styles.card} ${styles.transition} ${podiumNum == 0 ? "" : styles.podium + podiumNum}`
-  return (
-    <li className={cardClass} data-href={link}>
+const podiumClasses : {[id: number]: string} = {
+  0: "",
+  1: styles.podium1,
+  2: styles.podium2,
+  3: styles.podium3
+}
+
+export function CardTop({colour, image, podiumNum} : {colour: string, image: GridImageProps, podiumNum: number}) {
+  if (podiumNum > 0) {
+    return (
       <div className={styles.cardTop} style={{backgroundColor: colour}}>
         <GridImage path={image.path} alt={image.alt} />
       </div>
+    )
+  } else {
+    return <></>
+  }
+}
+
+export default function GridSquare({title, description, year, lang, colour, image, link, podiumNum = 0} : GridSquareProps) {
+  // let cardClass = "card transition" + (podiumNum == 0 ? "" : " podium" + podiumNum);
+  const cardClass = `${styles.card} ${styles.transition} ${podiumClasses[podiumNum]}`
+  return (
+    <li className={cardClass} data-href={link}>
+      <CardTop colour={colour} image={image} podiumNum={podiumNum}/>
       <div className={styles.cardBase}>
         <h2 className={styles.cardTitle} style={{color: colour}}>{title}</h2>
         <p className={styles.cardDescription}>{description}</p>
--- src/app/components/mainGrid.tsx
+++ src/app/components/mainGrid.tsx
@@ -1,7 +1,7 @@
 import styles from '../page.module.css'
 import GridSquare from './gridSquare';
 
-// import greenMapsLogo from ;
+// todo: fill alt text of svgs
 
 export default function MainGrid() {
   return (
@@ -9,7 +9,7 @@ export default function MainGrid() {
       <GridSquare 
         title="Green Maps"
         description="reducing vehicle emissions through&nbsp;smarter navigation"
-        year={2022}
+        year="2022"
         lang="Dart"
         colour="wheat"
         image={{
@@ -17,7 +17,85 @@ export default function MainGrid() {
           alt: "A leaf on a wheat background, the logo of the Green Maps project."
         }}
         link="./projects/greenmaps/"
+        podiumNum={2}
+      />
+      <GridSquare 
+        title="Yawning Detection"
+        description="applying deep learning to safety-critical environments"
+        year="2022-2023"
+        lang="Python, Dart"
+        colour="lightgreen"
+        image={{
+          path: "./assets/index/logo-yawnn.svg",
+          alt: ""
+        }}
+        link="./projects/yawnn/"
         podiumNum={1}
+      />
+      <GridSquare 
+        title="Maths Art"
+        description="exploring geometry at its finest"
+        year="2016 onwards"
+        lang="JS, Python"
+        colour="lightblue"
+        image={{
+          path: "./assets/index/logo-maths-art.svg",
+          alt: ""
+        }}
+        link="./maths-art/"
+        podiumNum={3}
+      />
+      <GridSquare 
+        title="jCompiler"
+        description="lexing and parsing an arbitrary input given a grammar"
+        year="2021-2022"
+        lang="OCaml, Java"
+        colour="#F9D1DA"
+        image={{
+          path: "",
+          alt: ""
+        }}
+        link="./projects/jcompiler/"
+        podiumNum={0}
+      />
+      <GridSquare 
+        title="Sentiment Classifier"
+        description="interpreting the emotion of reviews using various models"
+        year="2021"
+        lang="Java"
+        colour="#6EB5FF"
+        image={{
+          path: "",
+          alt: ""
+        }}
+        link="./projects/sentiment/"
+        podiumNum={0}
+      />
+      <GridSquare 
+        title="Pandemic Analysis"
+        description="simulating a pandemic and analysing real Covid data"
+        year="2020-2021"
+        lang="Python"
+        colour="lightcoral"
+        image={{
+          path: "",
+          alt: ""
+        }}
+        link="./projects/pandemic/"
+        podiumNum={0}
+      />
+      <GridSquare 
+        title="Hexad"
+        description="reaching for high scores by constructing hexagons"
+        year="2017-2019"
+        lang="Python"
+        colour="#C3B1E1"
+        image={{
+          path: "",
+          alt: ""
+        }}
+        link="https://play.google.com/store/apps/details?id=com.JBLabs.Hexad"
+        podiumNum={0}
       />
     </ol>
   )
