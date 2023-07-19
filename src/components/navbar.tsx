82e2c6a8d27330faac47cb4ea0e8bf8da44e743a
jacbn <jabrown405@gmail.com>
2023-07-19 22:11:07 +0100

    nav refix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,17 +6,19 @@ import Link from 'next/link'
 export default function NavBar({showName}: {showName: boolean}) {
   return (
     <nav>
-      {showName && (
       <div className={styles.myName}>
+      {showName && (
         <Link href="/">
           <span className={styles.titlePrimary}>Jacob </span> 
           <span className={styles.titleSecondary}> Brown</span>
         </Link>
-      </div>
       )}
-      <Link className={styles.navBox} href="/contact">Contact</Link>
-      <Link className={styles.navBox} href="/about">About</Link>
-      <Link className={styles.navBox} href="/">Home</Link>
+      </div>
+      <div>
+        <Link className={styles.navBox} href="/contact">Contact</Link>
+        <Link className={styles.navBox} href="/about">About</Link>
+        <Link className={styles.navBox} href="/">Home</Link>
+      </div>
     </nav>
   )
 }
