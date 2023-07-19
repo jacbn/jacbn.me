d1d2fbcba209fa8eda7d814e4643727a190b288a
jacbn <jabrown405@gmail.com>
2023-07-19 21:56:42 +0100

    added navbar to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import styles from '../styles/home.module.css'
 import Title from '../components/title'
 import MainGrid from '../components/mainGrid'
+import NavBar from '@/components/navbar'
 
 export default function Home() {
   return (
     <main>
       <Title />
+      <NavBar showName={false} />
       <div className={styles.subtitle}>
         My Projects:
       </div>
