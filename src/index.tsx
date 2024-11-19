45e93b3661de8f7f932f607dde7d00eaa2c4dfc3
jacbn <jabrown405@gmail.com>
2024-11-19 21:01:53 +0000

    Fix createRoot console warning

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import { createRoot } from 'react-dom/client';
 import App from "./App";
 import './styles.scss';
 
-const container = document.body;
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(<App />);
