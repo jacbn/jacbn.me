4467e0ddab2f9147d4026ac6af85bf5927cdf9b9
jacbn <jabrown405@gmail.com>
2024-11-19 20:59:28 +0000

    Fix tooltips class post-bootstrap

--- src/components/appIcon.tsx
+++ src/components/appIcon.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState } from "react";
 
 export default function AppIcon({image, href, hoverText} : {image: string, href?: string, hoverText?: string}) {
-  const containerStyle = `iconContainer ${(hoverText) ? "tooltip" : ''}`;
+  const containerStyle = `iconContainer ${(hoverText) ? "iconTooltip" : ''}`;
   const [active, setActive] = useState(false);
   const [hover, setHover] = useState(false);
   return (
@@ -13,5 +13,5 @@ export default function AppIcon({image, href, hoverText} : {image: string, href?
         {(hoverText) && <span className={`tooltipText ${(hover || active) ? "tooltipVisible" : "tooltipHidden"}`}>{hoverText}</span>}
       </a>
     </>
-  )
+  );
 }
