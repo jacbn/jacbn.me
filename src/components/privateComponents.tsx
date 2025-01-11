8b87d45e79fe4cf319519911dc19d735ea8fdc78
jacbn <jabrown405@gmail.com>
2025-01-11 14:49:05 +0000

    Fix build for private components

--- src/components/privateComponents.tsx
+++ src/components/privateComponents.tsx
@@ -1,17 +1,22 @@
 import React from "react";
 import { Suspense } from "react";
 
-// @ts-ignore this intentionally may not exist
-const isPrivate = await import("./private").then(module => module.isPrivateAccessible).catch(() => false);
-
 const privateComponent = (f : string) => {
     // @ts-expect-error we are not using the default export for these components
     const Component = React.lazy(() => import("./private").then(m => ({default: m[f]})));
-    return isPrivate 
-        ? <Suspense>
-            <Component/>
-        </Suspense>
-        : null;
+
+    const isPrivate = () => {
+        try {
+            // @ts-ignore
+            return (async () => await import("./private").then(module => module.isPrivateAccessible).catch(() => false))();
+        } catch (e) {
+            return false;
+        }
+    };
+
+    return <Suspense fallback={null}>
+        {isPrivate() && <Component/>}
+    </Suspense>;
 };
 
 export const MobileContact = () => privateComponent("MobileContact");
