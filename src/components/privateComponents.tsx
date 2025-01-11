5850bbfc3b5e5ca9c547703153ed2ad95ded5375
jacbn <jabrown405@gmail.com>
2025-01-11 19:29:59 +0000

    Attempt to fix Vercel problems

--- src/components/privateComponents.tsx
+++ src/components/privateComponents.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Suspense } from "react";
 
 const privateComponent = (f : string) => {
+    const isPrivate = import.meta.env.VITE_PRIVATE_ENABLED ?? false;
+    if (!isPrivate) return null;
+
     // @ts-expect-error we are not using the default export for these components
     const Component = React.lazy(() => import("./private").then(m => ({default: m[f]})));
 
-    const isPrivate = import.meta.env.VITE_PRIVATE_ENABLED ?? false;
-
     return <Suspense fallback={null}>
-        {isPrivate && <Component/>}
+        <Component/>
     </Suspense>;
 };
 
