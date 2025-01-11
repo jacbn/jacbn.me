d439cc110f1e2ef11737490d04795df0156460b6
jacbn <jabrown405@gmail.com>
2025-01-11 19:41:08 +0000

    Clean up public `.env`

--- src/components/privateComponents.tsx
+++ src/components/privateComponents.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import { Suspense } from "react";
 
 const privateComponent = (f : string) => {
-    const isPrivate = import.meta.env.VITE_PRIVATE_ENABLED ?? false;
-    if (!isPrivate) return null;
+    if (!import.meta.env.VITE_PRIVATE_COMPONENT) return null;
 
-    const Component = React.lazy(() => import.meta.env.VITE_PRIVATE_COMPONENT && import(import.meta.env.VITE_PRIVATE_COMPONENT).then(m => ({default: m[f]})));
+    const Component = React.lazy(() => import(import.meta.env.VITE_PRIVATE_COMPONENT).then(m => ({default: m[f]})));
 
     return <Suspense fallback={null}>
         <Component/>
